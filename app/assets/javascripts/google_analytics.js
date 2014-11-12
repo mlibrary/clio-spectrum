0f7287fadb9ae118b3e110cc23a74a6d0587560a
Marian Mosley <marian.mosley@gmail.com>
2014-11-12 09:40:13 -0500

    remove click listener from loop to prevent multiple events from being attached

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -42,37 +42,29 @@ $(document).ready(function() {
   // Apply to all off-site <A> tags, based on:
   //   http://www.electrictoolbox.com/jquery-open-offsite-links-new-window/
 
-  $('a[data-ga-category]').filter( function() {return this.hostname && this.hostname !== location.hostname} ).each(function() {
+  $('body').on('click', 'a[data-ga-category]', function(event) { // when someone clicks these links
+    // Gather up values at time of click, not at first load, to allow
+    // for ajax updates to, e.g., href labels or targets
 
-     var href   = $(this).attr("href");
-     var target = $(this).attr("target");
-     var text   = $(this).text();
-     console.log("found a.href href=["+href+"] text=["+text+"]")
+    var href   = $(this).attr("href");
+    var target = $(this).attr("target");
+    var text   = $(this).text();
 
-    $('body').on('click', 'a[data-ga-category]', function(event) { // when someone clicks these links
-      // Gather up values at time of click, not at first load, to allow
-      // for ajax updates to, e.g., href labels or targets
+    // The GA Category/Action may be given at a higher DOM level,
+    // e.g., at the root of an html menu/list of links, or a container div,
+    var category = $(this).closest("[data-ga-category]").data("ga-category") || "Outbound Link";
+    var action = $(this).closest("[data-ga-action]").data("ga-action") || "Click";
+    // Should the GA label default to the text or the URL?
+    var label = $(this).data("ga-label") || text;
 
-      var href   = $(this).attr("href");
-      var target = $(this).attr("target");
-      var text   = $(this).text();
+    event.preventDefault(); // don't open the link yet
 
-      // The GA Category/Action may be given at a higher DOM level,
-      // e.g., at the root of an html menu/list of links, or a container div,
-      var category = $(this).closest("[data-ga-category]").data("ga-category") || "Outbound Link";
-      var action = $(this).closest("[data-ga-action]").data("ga-action") || "Click";
-      // Should the GA label default to the text or the URL?
-      var label = $(this).data("ga-label") || text;
+    console.log("ga('send','event','"+category+"','"+action+"','"+label+"')")
+    ga('send', 'event', category, action, label);
 
-      event.preventDefault(); // don't open the link yet
-
-      console.log("ga('send','event','"+category+"','"+action+"','"+label+"')")
-      ga('send', 'event', category, action, label);
-
-      setTimeout(function() { // now wait 300 milliseconds...
-        window.open(href, (!target ? "_blank" : target)); // ...and open in new blank window
-      },300);
-    });
+    setTimeout(function() { // now wait 300 milliseconds...
+      window.open(href, (!target ? "_blank" : target)); // ...and open in new blank window
+    },300);
   });
 
 
