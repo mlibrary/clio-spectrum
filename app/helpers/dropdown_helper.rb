bc389f096814ac25c06733d16f80875ecaaf3ef3
Marian Mosley <marian.mosley@gmail.com>
2014-11-21 14:22:10 -0500

    use different helper methods for ga category for search results vs item detail

--- app/helpers/dropdown_helper.rb
+++ app/helpers/dropdown_helper.rb
@@ -1,63 +1,17 @@
 # encoding: utf-8
 
 module DropdownHelper
-  def send_to_email_dropdown_link(url)
-    return (link_to "Send to Email", '#', {'data-ga-category' => "Catalog Results List",
-                                          'data-ga-action' => "Selected Items",
-                                          'data-ga-label' => "Send to Email",
-                                          :href => url,
-                                          :id => "emailLink",
-                                          :name => "email",
-                                          :class => "lightboxLink",
-                                          :onclick => "return appendSelectedToURL(this);"})
-  end
-
-  def export_to_endnote_dropdown_link(url)
-    return (link_to "Export to EndNote", '#', {'data-ga-category' => "Catalog Results List",
-                                          'data-ga-action' => "Selected Items",
-                                          'data-ga-label' => "Export to EndNote",
-                                          :href => url,
-                                          :id => "endnoteLink",
-                                          :onclick => "return appendSelectedToURL(this);"})
-  end
-
-  def export_to_endnote_dropdown_link(url)
-    return (link_to "Export to EndNote", '#', {'data-ga-category' => "Catalog Results List",
-                                          'data-ga-action' => "Selected Items",
-                                          'data-ga-label' => "Export to EndNote",
-                                          :href => url,
-                                          :id => "endnoteLink",
-                                          :onclick => "return appendSelectedToURL(this);"})
-  end
-
-  def add_to_my_saved_list_dropdown_link
-    return (link_to "Add to My Saved List", '#', {'data-ga-category' => "Catalog Results List",
-                                          'data-ga-action' => "Selected Items",
-                                          'data-ga-label' => "Add to My Saved List",
-                                          :class => "saved_list_add",
-                                          :href => '#'})
-  end
-
-  def select_all_items_dropdown_link
-    return (link_to "Select All Items", '#', {'data-ga-category' => "Catalog Results List",
-                                        'data-ga-action' => "Selected Items",
-                                        'data-ga-label' => "Select All Items",
-                                        :href => '#',
-                                        :onclick => "selectAll(); return false;"})
-  end
-
-  def clear_all_items_dropdown_link
-    return (link_to "Clear All Items", '#', {'data-ga-category' => "Catalog Results List",
-                                        'data-ga-action' => "Selected Items",
-                                        'data-ga-label' => "Clear All Items",
-                                        :href => '#',
-                                        :onclick => "deselectAll(); return false;"})
-  end
-
-  def link_to_with_ga(text, uri, *options)
+  def link_to_item_detail_with_ga(text, uri, *options)
     options_hash = Hash[*options]
     options_hash.merge!('data-ga-category' =>  "Catalog Item Detail",
     'data-ga-action' =>  "Toolbar Click",
+    'data-ga-label' => text)
+    return (link_to text, uri, options_hash)
+  end
+  def link_to_item_list_with_ga(text, uri, *options)
+    options_hash = Hash[*options]
+    options_hash.merge!('data-ga-category' => ga_category_for_results_list,
+    'data-ga-action' =>  "Selected Items",
     'data-ga-label' => text)
     return (link_to text, uri, options_hash)
   end
--- spec/helpers/dropdown_helper_spec.rb
+++ spec/helpers/dropdown_helper_spec.rb
@@ -2,85 +2,9 @@ require 'spec_helper'
 
 describe DropdownHelper do
 
-  {
-    'Send to Email' => { 
-                        :id => "emailLink",
-                        'href' => "/catalog/email",
-                        :name => "email",
-                        :class => "lightboxLink",
-                        'data-ga-category' => "Catalog Results List",
-                        'data-ga-action' => "Selected Items",
-                        'data-ga-label' => "Send to Email",
-                        :onclick => "return appendSelectedToURL(this);"
-    },
-
-    'Export to EndNote' => {
-                            :id => 'endnoteLink',
-                            'href' => "/catalog/email",
-                            'data-ga-category' => "Catalog Results List",
-                            'data-ga-action' => "Selected Items",
-                            'data-ga-label' => "Export to EndNote",
-                            :onclick => "return appendSelectedToURL(this);"
-    },
-
-    'Add to My Saved List' => {
-                            :class => 'saved_list_add',
-                            'href' => '#',
-                            'data-ga-category' => "Catalog Results List",
-                            'data-ga-action' => "Selected Items",
-                            'data-ga-label' => "Add to My Saved List"
-    },
-
-    'Select All Items' => {
-                            :onclick => "selectAll(); return false;",
-                            'data-ga-category' => "Catalog Results List",
-                            'data-ga-action' => "Selected Items",
-                            'data-ga-label' => "Select All Items"
-    },
-
-    'Clear All Items' => {
-                            :onclick => "deselectAll(); return false;",
-                            'data-ga-category' => "Catalog Results List",
-                            'data-ga-action' => "Selected Items",
-                            'data-ga-label' => "Clear All Items"
-    }
-    }.each do |key1, val1|
-      method_name = "#{key1.gsub(/ /,'_').downcase}_dropdown_link"
-      url = ((key1 == 'Send to Email' || key1 == 'Export to EndNote') ? "/catalog/email" : nil)
-      describe "##{method_name}" do
-        val1.each do |key, val|
-        it "should have attribute #{key} with value #{val} on <a> tag" do
-          if url
-            link = send(method_name.to_sym, url)
-          else
-            link = send(method_name.to_sym)
-          end
-          expect(link).to match /<a [^>]*#{escape_parens(key.to_s)}=\"#{escape_parens(val)}\"/
-        end
-#      <a href="http://localhost:3000/catalog/email?id[]=4359539" class="lightboxLink" id="emailLink" name="email" onclick="return appendSelectedToURL(this);">Send to Email</a>
-        end
-        it "<a> tag should have text #{key1}" do
-          if url
-            link = send(method_name.to_sym, url)
-          else
-            link = send(method_name.to_sym)
-          end
-          expect(link).to match /<a [^>]*>#{key1}/
-        end
-        it "should have href to path" do
-          if url
-            link = send(method_name.to_sym, url)
-          else
-            link = send(method_name.to_sym)
-          end
-          expect(link).to match /<a [^>]*href=\"#{url}/
-        end
-      end
-    end
-
-  describe '#link_to_with_ga' do
+  describe '#link_to_item_detail_with_ga' do
     context 'Google Analytics tracking' do
-      let(:link){link_to_with_ga("MARC View", "catalog/9092438/librarian_view", {:id => 'librarian_link', :name => 'librarian_view'})}
+      let(:link){link_to_item_detail_with_ga("MARC View", "catalog/9092438/librarian_view", {:id => 'librarian_link', :name => 'librarian_view'})}
       context 'data-ga-category' do
         it{expect(link).to match /<a [^>]*data-ga-category=\"Catalog Item Detail\"/}
       end
@@ -102,6 +26,8 @@ describe DropdownHelper do
     end
   end
 end
+
 def escape_parens(str)
   str.gsub(/[()\[\]]/, '(' => '\(', ')' => '\)', '\[' => '\[', '\]' => '\]')
+end
 end
