0c36a48e4b7c7b6dfb28ba20c3b829f9f1150686
Marian Mosley <marian.mosley@gmail.com>
2014-11-26 16:54:33 -0500

    get helper spec working

--- spec/helpers/dropdown_helper_spec.rb
+++ spec/helpers/dropdown_helper_spec.rb
@@ -4,6 +4,7 @@ describe DropdownHelper do
 
   describe '#link_to_item_detail_with_ga' do
     context 'Google Analytics tracking' do
+      before{allow(self).to receive(:ga_category_for_item_detail).and_return('Catalog Item Detail')}
       let(:link){link_to_item_detail_with_ga("MARC View", "catalog/9092438/librarian_view", {:id => 'librarian_link', :name => 'librarian_view'})}
       context 'data-ga-category' do
         it{expect(link).to match /<a [^>]*data-ga-category=\"Catalog Item Detail\"/}
@@ -22,6 +23,28 @@ describe DropdownHelper do
       end
       context 'name' do
         it{expect(link).to match /<a [^>]*name=\"librarian_view\"/}
+      end
+    end
+  end
+
+  describe '#link_to_item_list_with_ga' do
+    context 'Google Analytics tracking' do
+      before{allow(self).to receive(:ga_category_for_results_list).and_return('Catalog Results List')}
+      let(:link){link_to_item_list_with_ga("Send to Email", "catalog/email", {:class => 'lightboxLink'})}
+      context 'data-ga-category' do
+        it{expect(link).to match /<a [^>]*data-ga-category=\"Catalog Results List\"/}
+      end
+      context 'data-ga-action' do
+        it{expect(link).to match /<a [^>]*data-ga-action=\"Selected Items\"/}
+      end
+      context 'data-ga-label' do
+        it{expect(link).to match /<a [^>]*data-ga-label=\"Send to Email\"/}
+      end
+      context 'href' do
+        it{expect(link).to match /<a [^>]*href=\"catalog\/email\"/}
+      end
+      context 'class' do
+        it{expect(link).to match /<a [^>]*class=\"lightboxLink\"/}
       end
     end
   end
