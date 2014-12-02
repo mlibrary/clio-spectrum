8b2e9ae0af08ac0dc950f70f2dc8743e8b8aa4a0
Marian Mosley <marian.mosley@gmail.com>
2014-12-02 15:02:03 -0500

    try useBeacon so that events will trigger even when a new page is loaded

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,38 +1,32 @@
-
-// OLDER CODE, COPIED FROM _common_head.html.haml
-// 
-// var _gaq = _gaq || [];
-// _gaq.push(['_setAccount', '#{GoogleAnalytics.web_property_id}']);
-// _gaq.push(['_setSiteSpeedSampleRate', 100]);
-// _gaq.push(['_trackPageview']);
-// 
-// (function() {
-//   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-//   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-//   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-// })();
-
-
-
 $(document).ready(function() {
 
   // UNIVERSAL TRACKING CODE FROM GA WEBSITE
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  (function(i,s,o,g,r,a,m){
+    i['GoogleAnalyticsObject']=r;
+
+    i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)
+    },
+
+    i[r].l=1*new Date();
+
+    a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];
+    a.async=1;
+    a.src=g;
+    m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   // Point our running instance to the appropriate GA property definition
-//  ga('create', google_analytics_web_property_id, 'columbia.edu');
+  ga('create', google_analytics_web_property_id, 'columbia.edu');
 
   // FOR LOCALHOST DEVELOPMENT USE THIS LINE INSTEAD
-  ga('create', google_analytics_web_property_id, 'none');
+//  ga('create', google_analytics_web_property_id, 'none');
 
   // This sends the a normal page-view to Google Analytics.
   // It runs once on page load.
   ga('send', 'pageview');
-
 
   $('body').on('submit', function(event) {
     var advanced = (event.target.getElementsByClassName("advanced_search_button").length == 1)
@@ -69,8 +63,10 @@ $(document).ready(function() {
     else{
       return
     }
+
     console.log("ga('send','event','"+category+"','"+action+"','"+label+"')")
-    ga('send', 'event', category, action, label);
+    ga('send', 'event', category, action, label, {useBeacon: true});
+
   });
 
   $('body').on('click', 'a[data-ga-category]', function(event) {
@@ -80,30 +76,34 @@ $(document).ready(function() {
     var href   = $(this).attr("href");
     var target = $(this).attr("target");
     var text   = $(this).text();
-    
+
     // The GA Category/Action may be given at a higher DOM level,
     // e.g., at the root of an html menu/list of links, or a container div,
+
     var category = $(this).closest("[data-ga-category]").data("ga-category") || "Outbound Link";
     var action = $(this).closest("[data-ga-action]").data("ga-action") || "Click";
-    // Should the GA label default to the text or the URL?
     var label = $(this).data("ga-label") || text;
+
     var open_new_window = false
+
     // Offsite links will open a new window unless it is a download link
 
     if ((this.hostname && this.hostname !== location.hostname) && action != "Download Click"){
-         open_new_window = true;
-         }
+      open_new_window = true;
+    }
 
     if (open_new_window){
       event.preventDefault(); // don't open the link yet
     }
+
     console.log("ga('send','event','"+category+"','"+action+"','"+label+"')")
-    ga('send', 'event', category, action, label);
-      if (open_new_window){
-        setTimeout(function() { // now wait 300 milliseconds...
-          window.open(href, (!target ? "_blank" : target)); // ...and open in new blank window
-        },300)
-      }
+    ga('send', 'event', category, action, label, {useBeacon: true});
+
+    if (open_new_window){
+      setTimeout(function() { // now wait 300 milliseconds...
+        window.open(href, (!target ? "_blank" : target)); // ...and open in new blank window
+      },300)
+    }
   });
 
 
