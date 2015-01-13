481bc43f402ab530a20f1867f15a9696a08b3883
Marian Mosley <marian.mosley@gmail.com>
2015-01-13 13:00:16 -0500

    refactor spec to eliminate soe dependencies and click with js and set wait time to 1000

--- spec/features/journals/journals_interface_spec.rb
+++ spec/features/journals/journals_interface_spec.rb
@@ -2,51 +2,72 @@ require 'spec_helper'
 
 describe 'E-Journals Search' do
 
-  it 'will be able to traverse next and previous links' do
-    visit journals_index_path('q' => 'notes')
+  context 'can traverse next and previous links' , js:true do
+    before :each do
+      visit journals_index_path('q' => 'notes')
+    end
+    context 'on search-results page' do
+      it{expect(page).not_to have_css('.index_toolbar a', text: 'Previous')}
+      it{expect(page).to have_css('.index_toolbar a', text: 'Next')}
+    end
 
-    page.should_not have_css('.index_toolbar a', text: 'Previous')
-    expect(page).to have_css('.index_toolbar a', text: 'Next')
+    context 'next results page' do
+      before{all('.index_toolbar a', text: 'Next').first.trigger('click')}
+      it{expect(page).to have_css('.index_toolbar a', text: 'Previous')}
+      it{expect(page).to have_css('.index_toolbar a', text: 'Next')}
+    end
 
-    all('.index_toolbar a', text: 'Next').first.click
-
-    expect(page).to have_css('.index_toolbar a', text: 'Previous')
-    expect(page).to have_css('.index_toolbar a', text: 'Next')
-
-    all('.index_toolbar a', text: 'Previous').first.click
-
-    page.should_not have_css('.index_toolbar a', text: 'Previous')
-    expect(page).to have_css('.index_toolbar a', text: 'Next')
+    context 'previous results page' do
+      before :each do
+        all('.index_toolbar a', text: 'Next').first.trigger('click')
+        all('.index_toolbar a', text: 'Previous').first.trigger('click')
+      end
+      it{expect(page).not_to have_css('.index_toolbar a', text: 'Previous')}
+      it{expect(page).to have_css('.index_toolbar a', text: 'Next')}
+    end
   end
 
-  it 'can move between item-detail and search-results', js: true do
-    visit journals_index_path('q' => 'letters')
+  context 'can move between item-detail and search-results', js: true do
+    before :each do
+      visit journals_index_path('q' => 'letters')
 
-    within all('.result.document').first do
-      all('a').first.click
+      within all('.result.document').first do
+        all('a').first.trigger('click')
+      end
     end
 
     # page.save_and_open_page # debug
 
-    expect(page).to have_css('#search_info', text: '1 of ')
-    page.should_not have_css('#search_info a', text: 'Previous')
-    expect(page).to have_css('#search_info a', text: 'Next')
+    context 'first item' do
+      it{expect(page).to have_css('#search_info', text: '1 of ')}
+      it{expect(page).not_to have_css('#search_info a', text: 'Previous')}
+      it{expect(page).to have_css('#search_info a', text: 'Next')}
+    end
 
-    find('#search_info a', text: 'Next').click
+    context 'next item' do
+      before :each do
+        expect(page).to have_css('#search_info a', text: 'Next')
+        find('#search_info a', text: 'Next').trigger('click')
+      end
+      it{expect(page).to have_css('#search_info', text: '2 of ')}
+      it{expect(page).to have_css('#search_info a', text: 'Previous')}
+      it{expect(page).to have_css('#search_info a', text: 'Next')}
+    end
 
-    expect(page).to have_css('#search_info', text: '2 of ')
-    expect(page).to have_css('#search_info a', text: 'Previous')
-    expect(page).to have_css('#search_info a', text: 'Next')
+    context 'previous item' do
+      before :each do
+        find('#search_info a', text: 'Next').trigger('click')
+        find('#search_info a', text: 'Previous').trigger('click')
+      end
+      it{expect(page).to have_css('#search_info', text: '1 of ')}
+      it{expect(page).not_to have_css('#search_info a', text: 'Previous')}
+      it{expect(page).to have_css('#search_info a', text: 'Next')}
+    end
 
-    find('#search_info a', text: 'Previous').click
-
-    expect(page).to have_css('#search_info', text: '1 of ')
-    page.should_not have_css('#search_info a', text: 'Previous')
-    expect(page).to have_css('#search_info a', text: 'Next')
-
-    find('#search_info a', text: 'Back to Results').click
-
-    expect(page).to have_css('.constraints-container', text: 'You searched for: letters')
+    context 'return to search results' do
+      before{find('#search_info a', text: 'Back to Results').trigger('click')}
+      it{expect(page).to have_css('.constraints-container', text: 'You searched for: letters')}
+    end
 
   end
 
