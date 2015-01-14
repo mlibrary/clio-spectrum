c053257b1dd4fd74434172b55d4f91a54c5d7f7b
Marian Mosley <marian.mosley@gmail.com>
2015-01-14 12:38:46 -0500

    move ga attributes upstream for item detail

--- app/helpers/dropdown_helper.rb
+++ app/helpers/dropdown_helper.rb
@@ -3,9 +3,6 @@
 module DropdownHelper
   def link_to_item_detail_with_ga(text, uri, *options)
     options_hash = Hash[*options]
-    options_hash.merge!('data-ga-category' =>  ga_category_for_item_detail,
-    'data-ga-action' =>  "Toolbar Click",
-    'data-ga-label' => text)
     return (link_to text, uri, options_hash)
   end
   def link_to_item_list_with_ga(text, uri, *options)
