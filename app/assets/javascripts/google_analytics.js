a8afe792b43c0c47644c8f69b42a6f2177883a4f
Marian Mosley <marian.mosley@gmail.com>
2015-01-29 15:52:36 -0500

    add class to dropdown items so ga will not skip them if they are internal links

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -8,10 +8,13 @@ $(document).ready(function() {
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   // Point our running instance to the appropriate GA property definition
-  ga('create', google_analytics_web_property_id, 'columbia.edu');
-
-  // FOR LOCALHOST DEVELOPMENT USE THIS LINE INSTEAD
-  // ga('create', google_analytics_web_property_id, 'none');
+  //
+  if ($("body").data("rails-env") == 'development'){
+    ga('create', google_analytics_web_property_id, 'none');
+  }
+  else{
+    ga('create', google_analytics_web_property_id, 'columbia.edu');
+  }
 
   // This sends the a normal page-view to Google Analytics.
   // It runs once on page load.
@@ -27,6 +30,12 @@ $(document).ready(function() {
   //   http://www.electrictoolbox.com/jquery-open-offsite-links-new-window/
 
   $(document).on('click', 'a', function(event) {
+
+    if (this.hostname && this.hostname === location.hostname){
+      if (!($(this).hasClass('dropdown-link'))){
+        return;
+      }
+    }
 
     // Gather up values at time of click, not at first load, to allow
     // for ajax updates to, e.g., href labels or targets
@@ -44,15 +53,15 @@ $(document).ready(function() {
 
     // if click also activates a page load, stash the values and submit when page reloads
 
-    if (action.match(/Display Options/)||action.match(/Sort by/)||label.match(/MARC View/)){
-      if (label != 'Export to EndNote'){
-        logToConsole("stashing ga('send','event','"+category+"','"+action+"','"+label+"')");
-        sessionStorage.setItem('data-ga-category', category);
-        sessionStorage.setItem('data-ga-action', action);
-        sessionStorage.setItem('data-ga-label', label);
-      }
-      return;
-    }
+  //  if (action.match(/Display Options/)||action.match(/Sort by/)||label.match(/MARC View/)){
+   //   if (label != 'Export to EndNote'){
+    //    logToConsole("stashing ga('send','event','"+category+"','"+action+"','"+label+"')");
+     //   sessionStorage.setItem('data-ga-category', category);
+      //  sessionStorage.setItem('data-ga-action', action);
+    //    sessionStorage.setItem('data-ga-label', label);
+     // }
+      //return;
+   // }
 
     var open_new_window = false;
     // Offsite links will open a new window unless it is a download link
@@ -96,76 +105,30 @@ $(document).ready(function() {
 
     var source = $(document).find('#sources').find('.datasource_link.selected').text();
 
-    // is it a search result page?
-
-
-    var category = "Search Results Load";
+    var category = "Zero Hits Search";
     var action = source;
     var label = document.URL;
     var advanced = false;
 
     // is there an event stashed away?
 
-    if (sessionStorage.getItem('data-ga-category')){
-      category = sessionStorage.getItem('data-ga-category');
-      action = sessionStorage.getItem('data-ga-action');
-      label = sessionStorage.getItem('data-ga-label');
-      logToConsole("sending stashed event: ga('send','event','"+category+"','"+action+"','"+label+"')");
-      ga('send', 'event', category, action, label, {useBeacon: true});
-      sessionStorage.removeItem('data-ga-category');
-      sessionStorage.removeItem('data-ga-action');
-      sessionStorage.removeItem('data-ga-label');
-      return;
-    }
+//    if (sessionStorage.getItem('data-ga-category')){
+//      category = sessionStorage.getItem('data-ga-category');
+//      action = sessionStorage.getItem('data-ga-action');
+//      label = sessionStorage.getItem('data-ga-label');
+//      logToConsole("sending stashed event: ga('send','event','"+category+"','"+action+"','"+label+"')");
+//      ga('send', 'event', category, action, label, {useBeacon: true});
+//      sessionStorage.removeItem('data-ga-category');
+//      sessionStorage.removeItem('data-ga-action');
+//      sessionStorage.removeItem('data-ga-label');
+//      return;
+//    }
 
-    if (!$('.result')) {
-      return;
-    }
-
-    if ((getQueryVariable('advanced_operator'))|| (getQueryVariable('form') == 'advanced')){
-      advanced = true;
-    }
     // zero-hits search?
 
     if ($('div.result_empty').length > 0){
-      category += " - Zero Hits";
-      if (advanced){
-        action = action + ' Advanced';
-      }
-      else{
-        action = action + ' Basic';
-      }
       logToConsole("ga('send','event','"+category+"','"+action+"','"+label+"')");
-      ga('send', 'event', category, action, label, {useBeacon: true});
-    }
-    else{
-
-      // return if it is not a search results page
-
-      if ($('.index_toolbar').length === 0 ){
-        return;
-      }
-
-      var hits = 0;
-      var num_hits = 0;
-
-      // Zero Hits Search?
-
-      if ($('.page_entries').length > 0){
-        num_hits = $('.page_entries').text().replace(/ /g,'').replace(/\|/g,'').split('of')[1].trim();
-      }
-      else {
-        num_hits = $('#current_item_info').text().replace(/ /g,'').replace(/\|/g,'').split('of')[1].trim();
-      }
-      if (advanced){
-        action = action + ' Advanced ('+ num_hits+' hits)';
-        advanced_operator = getQueryVariable('advanced_operator')|| ' OR ';
-      }
-      else{
-        action = action + ' Basic ('+ num_hits+' hits)';
-      }
-      logToConsole("ga('send','event','"+category+"','"+action+"','"+label+"')");
-      ga('send', 'event', category, action, label, {useBeacon: true});
+      ga('send', 'event', category, action, label);
     }
   });
 
@@ -184,30 +147,16 @@ $(document).ready(function() {
       if (source){
         src = source.split("_").map(function(i){return i[0].toUpperCase() + i.substring(1)}).join(" ");
       }
-      var category = "Search Results Load";
+      var category = "Zero Hits Search";
       var action = selected + ': ' + src;
       var label = document.URL;
 
       // Zero Hits Search?
 
       if ($(resp).find('div.result_empty').length > 0){
-        category += " - Zero Hits";
         logToConsole("ga('send','event','"+category+"','"+action+"','"+label+"')");
         ga('send', 'event', category, action, label, {useBeacon: true});
       }
-      else{
-        if (source){
-          num_hits = $(resp).find('div[data-source] > div.result_count').find('a > span.hidden-xs').text();
-          if (num_hits){
-            hits = num_hits;
-          }
-          action += ' ' + hits + " Hits";
-          label = settings.url;
-          logToConsole("ga('send','event','"+category+"','"+action+"','"+label+"')");
-          ga('send', 'event', category, action, label, {useBeacon: true});
-        }
-      }
-
     });
 
   });
@@ -219,13 +168,13 @@ $(document).ready(function() {
         console.log(message)
       }
   }
-function getQueryVariable(variable)
-{
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if(decodeURI(pair[0]) == variable){return decodeURI(pair[1]);}
-  }
-  return(false);
-}
+//function getQueryVariable(variable)
+//{
+//  var query = window.location.search.substring(1);
+//  var vars = query.split("&");
+//  for (var i=0;i<vars.length;i++) {
+//    var pair = vars[i].split("=");
+//    if(decodeURI(pair[0]) == variable){return decodeURI(pair[1]);}
+//  }
+// return(false);
+//}
--- spec/helpers/cul_catalog_helper_spec.rb
+++ spec/helpers/cul_catalog_helper_spec.rb
@@ -2,52 +2,6 @@ require 'spec_helper'
 
 describe CulCatalogHelper do
 
-  ['catalog', 'articles', 'academic_commons', 'journals', 'databases', 'library_web', 'archives', 'new_arrivals', 'newspapers'].each do |source|
-    context "#{source}" do
-      before {@active_source = source}
-      describe '#ga_category_for_results_list' do
-      end
-
-      context '#per_page_link' do
-        context 'no change to items per page' do
-          context 'href' do
-            it{expect(per_page_link('link.com', 10, 10)).to match /<a href=\"#\"/}
-          end
-          context 'data-ga-category' do
-            it{expect(per_page_link('link.com', 10, 10)).not_to match /data-ga-category/}
-          end
-        end
-
-        context 'change items per page' do
-          context 'should return the link with new number of pages ' do
-            context 'href' do
-              it{expect(per_page_link('link.com', 25, 10)).to match /<a [^>]*href=\"link.com\"/}
-            end
-            context 'per-page' do
-              it{expect(per_page_link('link.com', 25, 10)).to match /<a [^>]*per_page=\"25\"/}
-            end
-          end
-        end
-      end
-      context '#viewstyle_link' do
-        before{allow(self).to receive(:get_browser_option).and_return('compact_list')}
-        context 'no change to viewstyle' do
-          context 'href' do
-            it{expect(viewstyle_link('compact_list', 'Compact View')).to match /<a [^>]*href=\"#\"/}
-          end
-          context 'data-ga-category' do
-            it{expect(viewstyle_link('compact_list', 'Compact View')).not_to match /data-ga-category/}
-          end
-        end
-        context 'change from compact to standard view' do
-          context 'href' do
-            it{expect(viewstyle_link('standard_list', 'Standard View')).to match /<a [^>]*href=\"#\"/}
-          end
-        end
-      end
-    end
-  end
-
   describe '#ga_category_for_results_list' do
   {'catalog' => 'Catalog Results List',
    'articles' => 'Articles Results List',
